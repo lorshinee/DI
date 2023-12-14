cc3df31e02c9887155d4217dcbf6bc88c465c5f2
lorshinee <comaren.lorshinee@gmail.com>
2023-12-14 13:44:34 +0400

    Remove unused code

--- Week2/Day1/exercise1.js
+++ Week2/Day1/exercise1.js
@@ -1,8 +1,14 @@
+
+// exercise 1 
+
 const favoriteFood = "pasta";
 const favoriteMeal = "lunch";
 
 console.log("I eat "+favoriteFood+" at every "+favoriteMeal);
 console.log(`hbhbhb ${favoriteFood}  gvggg`)
+
+
+
 
 
 
